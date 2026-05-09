70e078878b04a978dbfb48e86267ea26c6388ece
Tijana Stankovic <153671519+tijanastankovic@users.noreply.github.com>
2026-05-09 14:20:28 +0200

    Implement UART transmit function using polling

--- src/uart.c
+++ src/uart.c
@@ -4,3 +4,13 @@ void uart_init(void)
 {
   UART_CR |= UART_ENABLE;
 }
+
+void uart_send_char(char c)
+{
+  while (!(UART_SR & UART_TX_READY)) // polling
+    {
+      // wait
+    }
+  UART_DR = c;
+}
+
